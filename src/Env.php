bf3f4bd87243c80b42f059d8f19db438ee03af55
EWake <dev@ewake.it>
2019-04-03 17:07:39 +0200

    Updated LOCAL_FIRST logic

--- src/Env.php
+++ src/Env.php
@@ -38,8 +38,8 @@ class Env
     {
         if (self::$options & self::USE_ENV_ARRAY) {
             $value = isset($_ENV[$name]) ? $_ENV[$name] : false;
-        } elseif ((self::$options & self::LOCAL_FIRST) && ($value = getenv($name, true)) !== false) {
-    	
+        } elseif ((self::$options & self::LOCAL_FIRST)) {
+    		$value = getenv($name, true) ?: getenv($name);
         } else {
             $value = getenv($name);
         }
